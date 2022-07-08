7b713d13e10ee0b8d9b254ed5e29735e42a11cd2
Nikolay Rodak <rodak1993@gmail.com>
2022-07-08 23:10:34 +0300

    finished work

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,8 +2,8 @@ public class CreditPaymentService {
 
     //**Ставка кредитования за месяц
     public double SetMonthPercent(double yearPercent) {
-        double interest;
-        return interest = yearPercent / (100 * 12);
+        double percent;
+        return percent = yearPercent / (100 * 12);
     }
     public double AnnuityFactor (double yearPercent, double creditPeriod) {
         double x = Math.pow(1+this.SetMonthPercent(yearPercent), -creditPeriod);
@@ -16,13 +16,5 @@ public class CreditPaymentService {
         return monthpay;
     }
 }
-//    public double calculate (double amount, double creditPeriod, double percent) {
 //
-////        return summCredit*(percent/((Math.pow(1-(1+percent),-timeOfCredit-1))));
-////        return summCredit*(percent*(1+percent)^timeOfCredit/((1+percent)^timeOfCredit-1));
-//
-//
-//        return amount*(percent*(1+percent)/((Math.pow(1-(1+percent),timeOfCredit-1))));
-//
-//    }
 
--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,36 @@ public class Main {
 
         CreditPaymentService service = new CreditPaymentService();
 
-
-        long amount = 1000000;
-        double year = 1;
-        double creditPeriod = year*12;
+        int amount = 10_000_000;
         double yearPercent = 9.99;
+        double year1 = 1;
+        double creditPeriod1 = year1*12;
+        double year2 = 2;
+        double creditPeriod2 = year2*12;
+        double year3= 3;
+        double creditPeriod3 = year3*12;
+
+
 
         double monthpercent = service.SetMonthPercent(yearPercent);
-        double annuity = service.AnnuityFactor(yearPercent, creditPeriod);
-        double monthpay = Math.round(service.SetMonthPay(amount,yearPercent,creditPeriod));
+        //1 год
+        double annuity1 = service.AnnuityFactor(yearPercent, creditPeriod1);
+        double monthpay1 = Math.round(service.SetMonthPay(amount,yearPercent,creditPeriod1));
+        //2 года
+        double annuity2 = service.AnnuityFactor(yearPercent, creditPeriod2);
+        double monthpay2 = Math.round(service.SetMonthPay(amount,yearPercent,creditPeriod2));
+        //3 года
+        double annuity3 = service.AnnuityFactor(yearPercent, creditPeriod3);
+        double monthpay3 = Math.round(service.SetMonthPay(amount,yearPercent,creditPeriod3));
 
+        System.out.println(
+                "Ежемесячный платёж на 1 год " + monthpay1
+                        + "\n" + "Ежемесячный платёж на 2 года " + monthpay2
+                        + "\n" + "Ежемесячный платёж на 3 года " + monthpay3);
 
-        System.out.println(annuity);
-        System.out.println(monthpay);
-        System.out.println(monthpercent);
+//       Использовались для дебагинга и сверки значений
+//        System.out.println("Ануитивное значение " + annuity1);
+//        System.out.println("Ежемесячный платёж " + monthpercent);
 
 
     }
