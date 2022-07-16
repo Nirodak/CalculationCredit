23d0033f578802cc7bedb05ccc54f9f7071ba05a
Nikolay Rodak <rodak1993@gmail.com>
2022-07-16 15:12:26 +0300

    refactor code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
 
-    public double SetMonthPay(double ammount, double yearPercent, double creditPeriod) {
+    public double setMonthPay(double ammount, double yearPercent, double creditPeriod) {
 
         double monthpay = ammount * (yearPercent / (100 * 12)) / (1 - Math.pow(1 + (yearPercent / (100 * 12)), -creditPeriod));
         return monthpay;
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         CreditPaymentService service = new CreditPaymentService();
 
         int amount = 1_000_000;
         double yearPercent = 9.99;
         double year1 = 1;
-        double creditPeriod1 = year1*12;
+        double creditPeriod1 = year1 * 12;
         double year2 = 2;
-        double creditPeriod2 = year2*12;
-        double year3= 3;
-        double creditPeriod3 = year3*12;
+        double creditPeriod2 = year2 * 12;
+        double year3 = 3;
+        double creditPeriod3 = year3 * 12;
 
 
         //1 год
-        double monthpay1 = Math.round(service.SetMonthPay(amount,yearPercent,creditPeriod1));
+        double monthpay1 = Math.round(service.setMonthPay(amount, yearPercent, creditPeriod1));
         //2 года
-        double monthpay2 = Math.round(service.SetMonthPay(amount,yearPercent,creditPeriod2));
+        double monthpay2 = Math.round(service.setMonthPay(amount, yearPercent, creditPeriod2));
         //3 года
-        double monthpay3 = Math.round(service.SetMonthPay(amount,yearPercent,creditPeriod3));
+        double monthpay3 = Math.round(service.setMonthPay(amount, yearPercent, creditPeriod3));
 
         System.out.println(
                 "Ежемесячный платёж на 1 год " + monthpay1
