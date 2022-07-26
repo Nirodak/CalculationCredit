5ae1f5c89347e1d0bdc5d0de0c4e75e9158d617e
Nikolay Rodak <rodak1993@gmail.com>
2022-07-26 12:44:05 +0300

    code fix

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,8 @@
 public class CreditPaymentService {
 
-    public double setMonthPay(double ammount, double yearPercent, double creditPeriod) {
+    public double setMonthPay(double amount, double yearPercent, double creditPeriod) {
 
-        double monthpay = ammount * (yearPercent / (100 * 12)) / (1 - Math.pow(1 + (yearPercent / (100 * 12)), -creditPeriod));
-        return monthpay;
+        return amount * (yearPercent / (100 * 12)) / (1 - Math.pow(1 + (yearPercent / (100 * 12)), -creditPeriod));
     }
 }
 
